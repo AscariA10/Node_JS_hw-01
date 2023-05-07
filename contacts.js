48b908481f071c1bba7dcbed6f48c9fc4b7b7abc
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2023-05-07 23:40:08 +0300

    1st commit

---
+++ contacts.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+
+/*
+ * Раскомментируй и запиши значение*/
+const contactsPath = async () => {};
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+   // ...твой код
+}
+
+function getContactById(contactId) {
+   // ...твой код
+}
+
+function removeContact(contactId) {
+   // ...твой код
+}
+
+function addContact(name, email, phone) {
+   // ...твой код
+}
